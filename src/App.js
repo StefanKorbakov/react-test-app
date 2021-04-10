34689dc081e17ab2f7b75d8343d4d211c97aa990
Stefan Korbakov <stefankorbakov@gmail.com>
2021-04-10 13:59:10 +0200

    Implemented input functionality

--- src/App.js
+++ src/App.js
@@ -18,14 +18,21 @@ class App extends Component {
         this.setState({persons: persons});
     };
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                { name: 'Max', age: 28 },
-                { name: event.target.value, age: 29 },
-                { name: 'Stephanie', age: 26 },
-            ],
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
         });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+        
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({persons: persons});
     };
 
     togglePersonsHandler = () => {
@@ -54,6 +61,7 @@ class App extends Component {
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
+                                changed={(event) => this.nameChangedHandler(event, person.id)}
                             />
                         );
                     })}
